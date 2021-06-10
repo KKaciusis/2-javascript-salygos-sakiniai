6d110ab6370018bbaacb7a240619d65ea70d2e78
KKaciusis <kkaciusis@yahoo.com>
2021-06-10 10:17:11 +0200

    kvadratines lygties sprendimas

--- 2/script.js
+++ 2/script.js
@@ -3,17 +3,16 @@
 let skaicius = 345
 let skaitmuo = Math.log(skaicius) * Math.LOG10E + 1 | 0;
 
-if(skaitmuo == 3) {
-var suma
-suma = 3 + 4 + 5;
-var vidurkis
-vidurkis = suma / 3;
-var sandauga
-sandauga = 3 * 4 * 5;
+let suma, vidurkis, sandauga;
+
+if (3 === skaitmuo) {
+    suma = 3 + 4 + 5;
+    vidurkis = suma / 3;
+    sandauga = 3 * 4 * 5;
 }
 
-console.log("skaicius",skaicius)
-console.log("skaitmuo",skaitmuo)
-console.log("suma",suma)
-console.log("vidurkis",vidurkis)
-console.log("sandauga",sandauga)
+console.log("skaicius", skaicius)
+console.log("skaitmuo", skaitmuo)
+console.log("suma", suma)
+console.log("vidurkis", vidurkis)
+console.log("sandauga", sandauga)
