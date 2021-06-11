edee12d207d533cd2dd037255e5bc9bd47a1dae0
KKaciusis <kkaciusis@yahoo.com>
2021-06-11 13:49:26 +0200

    second to HH/MM/SS

--- 1/script.js
+++ 1/script.js
@@ -1,6 +1,12 @@
-"use strict"
-var laikas = new Date(null);
-laikas.setSeconds(500); // sekundziu ivestis
-var suslaik = laikas.toISOString().substr(11, 8);
+"use strict";
 
-console.log(suslaik)
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+//let convertSecondsToTime = seconds => new Date(seconds * 1000).toISOString().substr(11, 8);
+
+function convertSecondsToTime(seconds) {
+    return new Date(seconds * 1000).toISOString().substr(11, 8);
+}
+
+document.getElementById('button').addEventListener('click', () => {
+    document.getElementById('result').innerHTML = convertSecondsToTime(document.getElementById('seconds').value);
+});
