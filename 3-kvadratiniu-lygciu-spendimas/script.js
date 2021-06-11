1b04b1ea8ca99178b0a82edb8879841e418ca188
KKaciusis <kkaciusis@yahoo.com>
2021-06-11 12:03:07 +0200

    Readability fix

--- 3-kvadratiniu-lygciu-spendimas/script.js
+++ 3-kvadratiniu-lygciu-spendimas/script.js
@@ -1,18 +1,23 @@
 "use strict";
-document.getElementById('calcbutton').addEventListener('click', () => {
+
+document.getElementById('calculateBtn').addEventListener('click', () => {
     let a1 = parseFloat(document.getElementById('a').value);
     let b1 = parseFloat(document.getElementById('b').value);
     let c1 = parseFloat(document.getElementById('c').value);
-    let rezultatas = document.getElementById('rezultatas');
-    let bm = Math.pow(b1, 2);
-    let daugybaac = (4 * a1 * c1);
-    let kvadratinesaknis = Math.sqrt(bm - daugybaac);
-    let lygtiesapacia = (2 * a1);
+
+    let result = document.getElementById('result');
+
+    let poweredNumber = Math.pow(b1, 2);
+    let multipliedAC = (4 * a1 * c1);
+    let squareRoot = Math.sqrt(poweredNumber - multipliedAC);
+    let equationBottom = (2 * a1);
+
     if (isNaN(kvadratinesaknis)) {
-        rezultatas.innerHTML = 'EIK PEKLAN';
+        result.innerHTML = 'GO PEKLAN';
     } else {
-        let res1 = (-b1 - kvadratinesaknis) / lygtiesapacia;
-        let res2 = (-b1 + kvadratinesaknis) / lygtiesapacia;
-        rezultatas.innerHTML = "x = " + res1 + " arba x = " + res2
+        let x1 = (-b1 - squareRoot) / equationBottom;
+        let x2 = (-b1 + squareRoot) / equationBottom;
+
+        result.innerHTML = "x = " + x1 + " or x = " + x2
     }
 });
