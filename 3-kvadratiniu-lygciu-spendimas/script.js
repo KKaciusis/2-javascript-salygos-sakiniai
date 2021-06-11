60ee9cf063eec0489f0677cfcb0fe124f11e3e0f
KKaciusis <kkaciusis@yahoo.com>
2021-06-11 11:33:12 +0200

    Squere equasion calculator

--- 3-kvadratiniu-lygciu-spendimas/script.js
+++ 3-kvadratiniu-lygciu-spendimas/script.js
@@ -1,37 +1,18 @@
-"use strict"
-
-let a = document.getElementById('a');
-let b = document.getElementById('b');
-let c = document.getElementById('c');
-let calcbutton = document.getElementById('calcbutton');
-let resultatas = document.getElementById('resultatas');
-
-calcbutton.addEventListener('click', calculate);
-
-function calculate() {
-    let a1 = a.value;
-    let b1 = b.value;
-    let c1 = c.value;
-    
-    a1 = parseFloat(a1);
-    b1 = parseFloat(b1);
-    c1 = parseFloat(c1);
-
+"use strict";
+document.getElementById('calcbutton').addEventListener('click', () => {
+    let a1 = parseFloat(document.getElementById('a').value);
+    let b1 = parseFloat(document.getElementById('b').value);
+    let c1 = parseFloat(document.getElementById('c').value);
+    let rezultatas = document.getElementById('rezultatas');
     let bm = Math.pow(b1, 2);
-    let daugac = (4 * a1 * c1);
-    let reskelimui = bm - daugac;
-    let kvsaknis = Math.sqrt(reskelimui);
-    let lygtiesbott = (2 * a1);
-
-    if (isNaN(kvsaknis) === true){
-        resultatas.innerHTML = 'EIK PEKLAN';
+    let daugybaac = (4 * a1 * c1);
+    let kvadratinesaknis = Math.sqrt(bm - daugybaac);
+    let lygtiesapacia = (2 * a1);
+    if (isNaN(kvadratinesaknis)) {
+        rezultatas.innerHTML = 'EIK PEKLAN';
+    } else {
+        let res1 = (-b1 - kvadratinesaknis) / lygtiesapacia;
+        let res2 = (-b1 + kvadratinesaknis) / lygtiesapacia;
+        rezultatas.innerHTML = "x = " + res1 + " arba x = " + res2
     }
-    else {
-        let res1 = (-b1 - kvsaknis) / lygtiesbott;
-        let res2 = (-b1 + kvsaknis) / lygtiesbott;
-        resultatas.innerHTML = "x = " + res1 + " arba x = " + res2
-    }
-
-
-
-}
+});
